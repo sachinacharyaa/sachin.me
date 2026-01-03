3ee559b49809fccfd47d933ae22ce65ee81fe630
sachinacharyaa <sachinacharyahancy@gmail.com>
2026-01-03 12:57:46 +0545

    hot-fix

---
+++ README.md
@@ -0,0 +1,6 @@
+This the official website of sachin, not tendulkar
+sachin acharya- who loves to be busy in solving bunch of codes!
+
+
+
+progress - 70%
---
+++ src/data/resume.tsx
@@ -0,0 +1,195 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon} from "lucide-react";
+
+export const DATA = {
+  name: "Sachin Acharya",
+  initials: "SA",
+  url: "https://sachin-acharya.com",
+  location: "Kathmandu, Nepal",
+  locationLink: "https://www.google.com/maps/place/kathmandu",
+  description:
+    " I love building things and sharing about it.",
+
+     summary: `
+I like technology — it matters.
+
+Probably living in terminal.
+
+Chatting with llms.
+`,
+
+
+
+  avatarUrl: "/me.jpeg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Postgres",
+    "Java",
+    "C++",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "thesachinacharya77@gmail.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://github.com/sachinacharyaa",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://linkedin.com/sachinacharyaa",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://x.com/sxchinacharya",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "mailto:sachinacharya77@gmail.com",
+        icon: Icons.email,
+        navbar: true,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Algoverse AI",
+      href: "https://algoverse.org",
+      badges: [],
+      location: "Remote",
+      title: "AI Researcher",
+      logoUrl: "/algoverse.png",
+      start: "Oct 2024",
+      end: "Feb 2025",
+      description:
+        "Paper about Strengthen Robutness",
+    },
+    // {
+    //   company: "Shopify",
+    //   badges: [],
+    //   href: "https://shopify.com",
+    //   location: "Remote",
+    //   title: "Software Engineer",
+    //   logoUrl: "/shopify.svg",
+    //   start: "January 2021",
+    //   end: "April 2021",
+    //   description:
+    //     "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+    // },
+    
+  ],
+  education: [
+    {
+      school: "Westcliff University",
+      href: "https://www.westcliff.edu/",
+      degree: "Bachelor's Degree of Computer Science (BSCS)",
+      logoUrl: "/westclif.png",
+      start: "2025",
+      end: "2028",
+    }
+    // {
+    //   company: "Nvidia",
+    //   href: "https://nvidia.com/",
+    //   badges: [],
+    //   location: "Santa Clara, CA",
+    //   title: "Software Engineer",
+    //   logoUrl: "/nvidia.png",
+    //   start: "January 2020",
+    //   end: "April 2020",
+    //   description:
+    //     "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
+    // },
+    // {
+    //   company: "Splunk",
+    //   href: "https://splunk.com",
+    //   badges: [],
+    //   location: "San Jose, CA",
+    //   title: "Software Engineer",
+    //   logoUrl: "/splunk.svg",
+    //   start: "January 2019",
+    //   end: "April 2019",
+    //   description:
+    //     "Co-developed a prototype iOS app with another intern in Swift for the new Splunk Phantom security orchestration product (later publicly demoed and launched at .conf annual conference in Las Vegas). Implemented a realtime service for the iOS app in Django (Python) and C++; serialized data using protobufs transmitted over gRPC resulting in an approximate 500% increase in data throughput.",
+    // },
+    // {
+    //   company: "Lime",
+    //   href: "https://li.me/",
+    //   badges: [],
+    //   location: "San Francisco, CA",
+    //   title: "Software Engineer",
+    //   logoUrl: "/lime.svg",
+    //   start: "January 2018",
+    //   end: "April 2018",
+    //   description:
+    //     "Proposed and implemented an internal ruby API for sending/receiving commands to scooters over LTE networks. Developed a fully automated bike firmware update system to handle asynchronous firmware updates of over 100,000+ scooters worldwide, and provide progress reports in real-time using React, Ruby on Rails, PostgreSQL and AWS EC2 saving hundreds of developer hours.",
+    // },
+    // {
+    //   company: "Mitre Media",
+    //   href: "https://mitremedia.com/",
+    //   badges: [],
+    //   location: "Toronto, ON",
+    //   title: "Software Engineer",
+    //   logoUrl: "/mitremedia.png",
+    //   start: "May 2017",
+    //   end: "August 2017",
+    //   description:
+    //     "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    // },
+  ],
+  
+  projects: [
+    // {
+    //   title: "Chat Collect",
+    //   href: "https://chatcollect.com",
+    //   dates: "Jan 2024 - Feb 2024",
+    //   active: true,
+    //   description:
+    //     "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+    //   technologies: [
+    //     "Next.js",
+    //     "Typescript",
+    //     "PostgreSQL",
+    //     "Prisma",
+    //     "TailwindCSS",
+    //     "Stripe",
+    //     "Shadcn UI",
+    //     "Magic UI",
+    //   ],
+    //   links: [
+    //     {
+    //       type: "Website",
+    //       href: "https://chatcollect.com",
+    //       icon: <Icons.globe className="size-3" />,
+    //     },
+    //   ],
+    //   image: "",
+    //   video:
+    //     "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    // },
+    // {
+
+  ],
+  hackathons: [
+   
+  ],
+  
+ 
+} as const;
